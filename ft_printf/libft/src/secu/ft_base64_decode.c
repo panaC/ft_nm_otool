337830a03c074e03f4585c54d8aab389428a3ea9
Pierre Leroux <panacpp@gmail.com>
2019-10-01 19:11:14 +0200

    printf libft

---
+++ ft_printf/libft/src/secu/ft_base64_decode.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_base64_decode.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pierre <pleroux@student.42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/30 20:20:05 by pierre            #+#    #+#             */
+/*   Updated: 2018/10/17 11:56:17 by pierre           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+t_string		ft_base64_decode(const t_string encod)
+{
+	t_string	s;
+
+	// IN PROGRESS , PLEASE DON'T LOOK THIS
+
+	s = (t_string)encod;
+	
+	//s = ft_strnew((len / 4 * 3) + 5);
+	return (s);
+}
